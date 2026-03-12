f8ad12b9b90c21813147f0bf8bbf6a6a3fd626b6
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-03-12 23:09:32 +0200

    navbar containing the menu and the headers

---
+++ src/component/Navbar.tsx
@@ -0,0 +1,27 @@
+import { NavLink } from "react-router-dom";
+export default function Navbar() {
+  return (
+    <>
+      <div className="w-40 h-full flex flex-col  pt-10 space-y-3 items-center bg-[#FFF7ED] text-black font-semibold">
+        <NavLink
+          to="/"
+          end
+          className={({ isActive }) =>
+            `${isActive ? "underline text-green-600" : ""} hover:underline`
+          }
+        >
+          Dashboard
+        </NavLink>
+        <NavLink
+          to="transactions"
+          end
+          className={({ isActive }) =>
+            `${isActive ? "underline text-green-600" : ""} hover:underline`
+          }
+        >
+          Ledger
+        </NavLink>
+      </div>
+    </>
+  );
+}

783c1eed055d330f37c9c45859c66168ae972a1e
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-03-12 23:12:58 +0200

    added a createbrowser router to create the routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
-import _reactLogo from './assets/react.svg'
-import _viteLogo from '/vite.svg'
-import './App.css'
-
+import _reactLogo from "./assets/react.svg";
+import _viteLogo from "/vite.svg";
+import "./App.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Forms, { action as formAction } from "./pages/Forms";
+import Ledger from "./pages/Ledger";
+import Layout from "./component/Layout";
+import TransactionLayout from "./pages/TransactionLayout";
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route element={<Layout />}>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="transactions" element={<TransactionLayout />}>
+          <Route index element={<Ledger />} />
+          <Route path="add" element={<Forms />} action={formAction} />
+        </Route>
+      </Route>,
+    ),
+  );
 
   return (
     <>
-      
+      <div className="w-200 h-150 flex mt-5 mx-auto bg-[#475868]">
+        <RouterProvider router={router} />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

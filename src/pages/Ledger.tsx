0826cad4d6b18e36803c6e6ecdd8374434d22615
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-03-13 20:26:20 +0200

    added the filtering function

--- src/pages/Ledger.tsx
+++ src/pages/Ledger.tsx
@@ -1,11 +1,27 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet, useSearchParams } from "react-router-dom";
 import type { Transactions } from "../assets/types";
 export default function Ledger() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  let typeFilter = searchParams.get("type");
   const currentTransactions = JSON.parse(
     localStorage.getItem("transaction") || "[]",
   );
 
-  const transaction = currentTransactions.map((el : Transactions) => (
+  const handleFilter = (type: string, value: string | null) => {
+    setSearchParams((prevValue) => {
+      if (value === null) {
+        prevValue.delete("type");
+      } else {
+        prevValue.set(type, value);
+      }
+      return prevValue;
+    });
+  };
+  const displayTransactions = typeFilter
+    ? currentTransactions.filter((el: Transactions) => el.type === typeFilter)
+    : currentTransactions;
+
+  const transaction = displayTransactions.map((el: Transactions) => (
     <tr key={el.id} className="border-b hover:bg-gray-50 transition-colors">
       <td className="px-4 py-3 font-medium text-gray-700">{el.description}</td>
 
@@ -34,13 +50,16 @@ export default function Ledger() {
       <td className="px-4 py-3 text-gray-500">{el.date}</td>
 
       <td className="px-4 py-3">
-        <button className="text-sm bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded-md transition">
+        <NavLink
+          to={`add/${el.id}`}
+          onClick={() => el.id}
+          className="text-sm bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded-md transition"
+        >
           Delete
-        </button>
+        </NavLink>
       </td>
     </tr>
   ));
-
   return (
     <>
       <div className="mt-5 flex flex-col space-y-5 w-180 mx-4">
@@ -53,7 +72,28 @@ export default function Ledger() {
             Add Transaction
           </NavLink>
         </div>
-
+        {transaction.length > 0 && (
+          <div className="w-full flex space-x-2   font-semibold">
+            <button
+              className={`${typeFilter === "income" ? "text-amber-200 underline" : "text-white"} cursor-pointer p-1 flex items-center rounded-md`}
+              onClick={() => handleFilter("type", "income")}
+            >
+              Income
+            </button>
+            <button
+              className={`${typeFilter === "expense" ? "text-amber-200 underline" : "text-white"} cursor-pointer p-1 flex items-center rounded-md`}
+              onClick={() => handleFilter("type", "expense")}
+            >
+              Expense
+            </button>
+            <button
+              className={`cursor-pointer p-1 flex items-center rounded-md`}
+              onClick={() => handleFilter("type", null)}
+            >
+              Clear
+            </button>
+          </div>
+        )}
         {transaction.length > 0 && (
           <div className="overflow-hidden border border-gray-200 rounded-lg shadow-sm">
             <table className="w-full text-left text-sm">

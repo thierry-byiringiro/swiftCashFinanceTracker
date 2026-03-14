2a3f1352a7141e30f6faffba9a25cfb5c45d53d4
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-03-14 23:56:12 +0200

    added new class utilities

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 export default function Navbar() {
   return (
     <>
-      <div className="w-40 h-full flex flex-col  pt-10 space-y-3 items-center bg-[#FFF7ED] text-black font-semibold">
+      <div className="w-40 h-full flex flex-col  pt-10 space-y-3 items-center bg-[#FFF7ED] text-black font-semibold  rounded-bl-md rounded-tl-md">
         <NavLink
           to="/"
           end
